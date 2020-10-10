b3f4222abdaef25e022bddfac153efcc30ba18d2
Shashank Menon <massmenon@gmail.com>
2020-10-10 21:25:08 +0530

    Updates

---
+++ precache-manifest.75c60001a73b2765dcf36dab852847a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e19666e2b9064bc02a2cb8927847126",
+    "url": "/spaceapp/index.html"
+  },
+  {
+    "revision": "b4313ff5562422feecc1",
+    "url": "/spaceapp/static/css/main.f541148e.chunk.css"
+  },
+  {
+    "revision": "a894a0108d62c3e33783",
+    "url": "/spaceapp/static/js/2.70bb85cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spaceapp/static/js/2.70bb85cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4313ff5562422feecc1",
+    "url": "/spaceapp/static/js/main.8e69bfab.chunk.js"
+  },
+  {
+    "revision": "78de23e3a798fbdbc2fa",
+    "url": "/spaceapp/static/js/runtime-main.74fb6372.js"
+  }
+]);
